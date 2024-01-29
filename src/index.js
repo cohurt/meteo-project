093495f6adc3ddc2fb8b178cc5c49863f0f3b1fc
cohurt <157282165+cohurt@users.noreply.github.com>
2024-01-29 21:19:41 +0100

    api integration city and temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function displayWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+function searchCity(city) {
+  let apiKey = "83aa0a1668bf9d1b24632c2o6770582t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
 function submitCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitCitySearch);
+
+searchCity("London");
